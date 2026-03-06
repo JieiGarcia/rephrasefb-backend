eb418bcf06912181eb3b8365cd227d668945ef6a
JieiGarcia <jiei.aca@gmail.com>
2026-03-06 16:18:56 +0900

    Create API server

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
+	"net/http" // web server function
 
+	"github.com/go-chi/chi/v5" // add router
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
-	dsn := "postgres://user:password@localhost:5432/writing_app?sslmode=disable"
+	dsn := "postgres://user:password@localhost:5432/rephrasefb?sslmode=disable"
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		log.Fatalf("failed to connect database: %v", err)
@@ -52,4 +55,48 @@ func main() {
 	}
 
 	log.Println("migration completed successfully")
+
+	// API server
+
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("OK! RephraseFB API server is running"))
+	})
+
+	r.Post("/users", func(w http.ResponseWriter, r *http.Request) {
+		type CreateUserRequest struct {
+			ExternalUserID string `json:"externalUserId"`
+		}
+		var req CreateUserRequest
+
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		// INSERT
+		var newID string
+		err := db.QueryRow(
+			"INSERT INTO users (external_user_id) VALUES ($1) RETURNING id",
+			req.ExternalUserID,
+		).Scan(&newID)
+
+		if err != nil {
+			log.Printf("failed to insert user: %v", err)
+			http.Error(w, "failed to create user", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"id":      newID,
+			"message": "user created successfully",
+		})
+	})
+
+	log.Println("Server starting on port 8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 }
